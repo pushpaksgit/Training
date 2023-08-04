4f7f662624234b43d5036f7ff9a976279cab4eb9
pushpaksgit <141202782+pushpaksgit@users.noreply.github.com>
2023-08-04 10:14:33 +0530

    Update InsertationSort.java

--- Day3/InsertationSort.java
+++ Day3/InsertationSort.java
@@ -1,1 +1,20 @@
+package Training.Day3;
+import java.util.*;
+public class InsertationSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arr[]={2,8,1,4,7,4,2};
+        int key;
+        for(int i=0;i<arr.length;i++){
+            key=arr[i];
+            int j=i-1;
+            while(j>=0 && key<arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
 
